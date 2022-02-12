7172afb973649c05bb17d0f7d80ca77667dc7ad8
Khánh Vi Nguyễn <vicntt10@gmail.com>
2022-02-12 15:50:45 +0700

    create

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    mode: "jit",
+    content: [
+        "./index.html",
+        "./src/**/*.{vue,js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                quicksand: ["Quicksand_Regular"],
+                quicksand_bold: ["Quicksand_Bold"]
+            },
+        },
+    },
+    plugins: [],
+}
