3c355015b04a11318af198a17366991cecb7359f
Khánh Vi Nguyễn <vicntt10@gmail.com>
2022-07-19 16:23:25 +0700

    test 12

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -136,7 +136,7 @@ function Home() {
         </div>
         <div className="py-10 mb-10">
           <h1 className="relative font-quicksand_bold text-[30px] text-[#fa348c] text-center before:absolute before:content-[''] before:w-[100px] before:h-[2px] before:bg-[#fb66a8] before:left-[50%] before:top-[130%] before:translate-x-[-50%] before:translate-y-[-50%] after:absolute after:content-[''] after:w-[10px] after:h-[10px] after:border-2 after:rounded-[50%] after:bg-white after:left-[50%] after:top-[130%] after:translate-x-[-50%] after:translate-y-[-50%] ">
-            ĐIỀU GÌ KHIẾN PHỤ HUYNH TIN TƯỞNG COOL SCHOOL?
+            ĐIỀU GÌ KHIẾN PHỤ HUYNH TIN TƯỞNG COOL SCHOOL?12
           </h1>
         </div>
       </div>
